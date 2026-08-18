0c5a5854dab89ed4595dfdb14d19b6c93dc8e6d8
VaibhaviMasal <142891383+VaibhaviMasal@users.noreply.github.com>
2026-08-18 19:49:05 +0530

    interview repo

---
+++ CareerConnect.Application/Features/Interviews/Interfaces/IInterviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using CareerConnect.Domain.Entities;
+
+namespace CareerConnect.Application.Features.Interviews.Interfaces;
+
+public interface IInterviewRepository
+{
+    Task AddAsync(InterviewSchedule interview);
+    Task<List<InterviewSchedule>> GetByApplicationIdAsync(int applicationId);
+    Task<List<InterviewSchedule>> GetAllAsync();
+    Task<InterviewSchedule?> GetByIdAsync(int id);
+    Task UpdateAsync(InterviewSchedule interview);
+    Task DeleteAsync(InterviewSchedule interview);
+}
---
+++ CareerConnect.Infrastructure/Repositories/InterviewRepository.cs
@@ -0,0 +1,51 @@
+﻿using CareerConnect.Application.Features.Interviews.Interfaces;
+using CareerConnect.Domain.Entities;
+using CareerConnect.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CareerConnect.Infrastructure.Repositories;
+
+public class InterviewRepository : IInterviewRepository
+{
+    private readonly CareerConnectDbContext _context;
+
+    public InterviewRepository(CareerConnectDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(InterviewSchedule interview)
+    {
+        await _context.InterviewSchedules.AddAsync(interview);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<InterviewSchedule>> GetByApplicationIdAsync(int applicationId)
+    {
+        return await _context.InterviewSchedules
+            .Where(i => i.ApplicationId == applicationId)
+            .ToListAsync();
+    }
+
+    public async Task<List<InterviewSchedule>> GetAllAsync()
+    {
+        return await _context.InterviewSchedules.ToListAsync();
+    }
+
+    public async Task<InterviewSchedule?> GetByIdAsync(int id)
+    {
+        return await _context.InterviewSchedules.FindAsync(id);
+    }
+
+    public async Task UpdateAsync(InterviewSchedule interview)
+    {
+        _context.InterviewSchedules.Update(interview);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(InterviewSchedule interview)
+    {
+        _context.InterviewSchedules.Remove(interview);
+        await _context.SaveChangesAsync();
+    }
+}
